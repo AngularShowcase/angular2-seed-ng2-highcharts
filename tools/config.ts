c9714aa8b2cb524aee1254d022ff892ed5cbada2
Richard Natal <Richard Natal>
2016-02-16 00:07:01 -0200

    Setup ng2-highcharts to the build.prod environment

--- tools/config.ts
+++ tools/config.ts
@@ -126,7 +126,8 @@ export const SYSTEM_BUILDER_CONFIG = {
   paths: {
     '*': `${TMP_DIR}/*`,
     'angular2/*': 'node_modules/angular2/*',
-    'rxjs/*': 'node_modules/rxjs/*'
+    'rxjs/*': 'node_modules/rxjs/*',
+    'ng2-highcharts/*': 'node_modules/ng2-highcharts/*'
   }
 };
 
